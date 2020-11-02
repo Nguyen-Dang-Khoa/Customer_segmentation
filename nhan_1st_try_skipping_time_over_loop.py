5e172967078054ca620192b3f1e95433eb1c9572
Nguyen-Dang-Khoa <58618740+Nguyen-Dang-Khoa@users.noreply.github.com>
2020-11-02 20:03:15 +0700

    Update nhan_1st_try_skipping_time_over_loop.py

--- nhan_1st_try_skipping_time_over_loop.py
+++ nhan_1st_try_skipping_time_over_loop.py
@@ -1,130 +1,190 @@
-# ### 0. Packages Installation!
-# !pip install selenium
-# !pip install parsel
-# **Install chrome**s
-# !apt-get update # to update ubuntu to correctly run apt install
-# !apt install -y chromium-chromedriver
-
-# ### 1.1 Opening Linkedin with `webdriver.Chrome()`
-import time 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-t0 = time.time()
-driver = r'C:\Users\Admin\Downloads\chromedriver_win32\chromedriver' # should be replaced by the path to your chromedriver
-driver = webdriver.Chrome(driver)
-driver.get('https://www.linkedin.com')
-username = driver.find_element_by_name('session_key')
-username.send_keys('thanosteamk09@gmail.com')
-password = driver.find_element_by_name('session_password')
-password.send_keys('thaNos99*')
-log_in_button = driver.find_element_by_class_name('sign-in-form__submit-button')
-log_in_button.click()
-
-# ### 1.2. Import the `Selector` from `parsel` for scrapping linkedin accounts.
-## Step 1. Idea of stoping-time-in-while,
-for k in range(20):
-    t0 = time.time()                         ## index in your iteration
-    fk = np.uint8(k**3 + k**2 + k + 1)       ## function defined w.r.t the index
-    fk = fk // 121 
-    print('k = %s, fk = %s, time_k = %s'%(k, fk, time.time() - t0));  ## measure the time-out of function in each step
-    if (k+1)%10 == 0:
-        t0 = time.time()
-        wait_time = 5   ## timeout variable [seconds] can be omitted, if you use specific value in the while condition
-        while time.time() < t0 + wait_time:
-            stop = 0
-            if stop == 5:
-                break
-            stop -= 1
-        print('stoping_time = ',time.time() - t0)
-    elif k==20:
-        print('Done! complete the short-testing!')
-
-##### Step 2. Which `url link` is used to scrap?
-from parsel import Selector
-## List of url link
-links = ['https://www.linkedin.com/in/do-nhan-39a4191a3/',
-        'https://uk.linkedin.com/in/thornbeck',
-        'https://www.linkedin.com/in/brandon-foxworth-793a4980/',
-        'https://www.linkedin.com/in/steve-benckenstein/',
-        'https://www.linkedin.com/in/minh-hieu-do-82175740/',
-         'https://uk.linkedin.com/in/pauljgarner', 
-         'https://www.linkedin.com/in/lacy-judd-ba845273/',
-         'https://www.linkedin.com/in/daniel-reyes-367b64b2/',
-         'https://www.linkedin.com/in/thoa-thieu-294252135/',
-         'https://uk.linkedin.com/in/eastwoodalex', 
-         'https://www.linkedin.com/in/lewis-forsyth-49333284/',
-         'https://www.linkedin.com/in/hoang-ha-84b31041/',
-         'https://uk.linkedin.com/in/navaneetham', 
-         'https://www.linkedin.com/in/ltp238/',
-         'https://www.linkedin.com/in/hoan-huynh-8b3ba9b6', 
-         'https://www.linkedin.com/in/yoelohayon', 
-         'https://www.linkedin.com/in/tknguyen2015/',
-         'https://www.linkedin.com/in/siddharthsatpathy',
-         'https://www.linkedin.com/in/thien-trang-bui/',
-         'https://www.linkedin.com/in/david-williams-9a1392153/',
-         'https://www.linkedin.com/in/jonathanhvan/',
-         'https://www.linkedin.com/in/stephen-long-b786b71ab/',
-         'https://www.linkedin.com/in/kayla-lee-410053a8/'
-         ]
-
-## initialize empty list
-names = []
-jobs = []
-company = []
-education = []
-location = []
-school = []
-
-# **Step 3: finding `xpath`** and using skipping-time
-# For example, you can find the infomation of the `job title` by searching the `inspect` then copy its `full xpath`
-## append the obtained values from Selector function
-T0 = time.time()
-for k in range(len(links)):
-    t0 = time.time()
-    path = links[k]
-    driver.get(path)
-    sel = Selector(text=driver.page_source)     
-    names.append(sel.xpath("/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[1]/section/div[2]/div[2]/div[1]/ul[1]/li[1]/text()").extract_first())
-    jobs.append(  sel.xpath("/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[1]/section/div[2]/div[2]/div[1]/h2/text()").extract_first() )
-    company.append(sel.xpath('/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[1]/section/div[2]/div[2]/div[2]/ul/li[1]/a/span/text()').extract_first())
-    education.append(sel.xpath('/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[2]/div[5]/span/div/section/div[2]/section/ul/li[1]/div/div/a/div[2]/div/p[1]/span[2]/text()').extract_first())
-    location.append(sel.xpath('/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[1]/section/div[2]/div[2]/div[1]/ul[2]/li[1]/text()').extract_first())
-    school.append(sel.xpath('/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[2]/div[5]/span/div/section/div[2]/section/ul/li[1]/div/div/a/div[2]/div/h3/text()').extract_first())
-    print("step = %s, appending complete after %s (seconds) "%(k, time.time() - t0))
-    if (k+1) % 10 == 0:
-        t0 = time.time()
-        wait_time = 50   ## timeout variable [seconds] can be omitted, if you use specific value in the while condition
-        while time.time() < t0 + wait_time:
-            stop = 0
-            if stop == 50:
-                break
-            stop -= 1
-        print('stoping_time = ',time.time() - t0)
-print('Attaching progress : Completed!!! \t total url_links = %s, total_time = %s'%(len(links), time.time() - T0)))
-
-## Step 4. Create dataframe
 import pandas as pd
-df = pd.DataFrame({'name': names, 
-                   'job title': jobs, 
-                   'company': company, 
-                   'education': education, 
-                   'school': school, 
-                   'location': location, 
-                   'url': links}) 
-df
-## Text-processing
-def remove(x):
-    if x == None:
-        x = None
-    else:
-        x = x.replace('\n\t', '').replace('\n', '')
-    return x
-for col in df.columns:
-    df[col] = df[col].apply(lambda x : remove(x))
-df
+# import web driver
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.keys import Keys #  [Keys.LEFT, Keys.DOWN, Keys.RIGHT, Keys.UP]
+from parsel import Selector
+from shutil import which
+from time import sleep
+import time
+ 
+class LinkedIn_URL:
+    '''
+    get linked in URL by using outlook
+    '''
+    def __init__(self, url):
+        # specifies the path to the chromedriver.exe
+        self.driver = webdriver.Chrome(executable_path=which("chromedriver"))
+        self.driver.get(url)
+    
+    def get_site_info(self):
+        # get the site information
+        print('URL:', self.driver.current_url)
+        print('Title:', self.driver.title)
+        sleep(2)
+        self.driver.save_screenshot('screen_shot.png')
 
-# ## 3. Save & verifying saved-`csv.file`
-#df.to_csv('linkedin.csv')
+    def outlook_signin(self, username, password):
+        # sign in website
+        self.selector_click('body > header > div > aside > div > nav > ul > li:nth-child(2) > a')
 
-## Display saved-file
-#pd.read_csv('linkedin.csv')
+        # input email
+        username_loc = self.driver.find_element_by_css_selector('#i0116')
+        username_loc.send_keys(username)
+        username_loc.send_keys(Keys.ENTER)
+        sleep(1)
+
+        # input password
+        password_loc = self.driver.find_element_by_css_selector('#i0118')
+        password_loc.send_keys(password)
+        sleep(0.5)
+        self.selector_click('#idSIButton9')
+
+        # Sign in yes button
+        self.selector_click('#idSIButton9')
+        sleep(0.5)
+
+    def linked_in_signin(self, username, password):
+        # Save current_window as the main_window
+        main_window = self.driver.current_window_handle
+        # Open new-tab
+        self.driver.execute_script("window.open();")
+        self.driver.switch_to_window(self.driver.window_handles[1])
+        self.driver.get('https://www.linkedin.com')
+        sleep(2)
+
+        # Log-in Linkedin in the second TAB
+        username = self.driver.find_element_by_name('session_key')
+        username.send_keys(username)
+        password = self.driver.find_element_by_name('session_password')
+        password.send_keys(password)
+        self.class_name_click('sign-in-form__submit-button')
+        sleep(1)
+
+        # Switch back to main window
+        self.driver.switch_to_window(main_window)
+        sleep(1)
+
+    def get_url(self, email_list):
+        # choose people contact
+        self.selector_click('#app > div > div._3KAPMPOz8KsW24ooeUflK2 > div > div._1TpU2KF6f_EeQiytBaYj8I > div._25oA4qBLP_b6P080cw5s2H.css-43 > div._1_iewjAgd8BRIHH3zHMH1V > div > div > div:nth-child(3) > div > a > span')
+        ## loop though contact list
+        # select first contact
+        self.selector_click('#HubPersonaId_AQQkADAwATNiZmYAZC0zMwBkYS1jNDQANS0wMAItMDAKABAA\/0L4nPVSf0WielUcdLGSkw\=\= > div > div._3qxq4FbPD_ovy_o1o8-N4E')
+        # select LinkedIn button
+        self.xpath_click('/html/body/div[2]/div/div[2]/div/div[3]/div/div[2]/div/div[2]/div/div/div/div/div[1]/div/div/div/button[4]')
+        # Loop email over email_contact
+        linkedin_url = []
+        search_query = self.driver.find_element_by_xpath('//*[@id="SearchBox91"]')  ## change each-time you log-in
+        for mail in email_list:
+            ## Enter & access to search_box of the email_contact
+            search_query = self.driver.find_element_by_xpath('//*[@id="SearchBox91"]')
+            search_query.send_keys(mail)
+            search_query.send_keys(Keys.ENTER)
+
+            ## Find the button of Linkedin-tab by name 
+            contact_button = self.driver.find_element_by_xpath('//*[class = "ms-Pivot-text-349 text-354"]')
+            contact_button.click()  
+
+            ## Access the button : "See full profile on Linkedin"
+            contact_button = self.driver.find_element_by_name("ViewProfileInLinkedIn")
+            contact_button.click()  
+
+            ## get the url-link
+            link = self.driver.current_url
+            linkedin_url.append(link)
+        return linkedin_url
+
+    def selector_click(self, css_selector):
+        self.driver.find_element_by_css_selector(css_selector).click()
+
+    def xpath_click(self, xpath):
+        self.driver.find_element_by_xpath(xpath).click()
+    
+    def class_name_click(self, class_name):
+        self.driver.find_element_by_class_name(class_name).click()
+
+
+class Url_Scrap:
+    def __init__(self, linked_in_url, url_list):
+        # specifies the path to the chromedriver.exe
+        self.driver = webdriver.Chrome(executable_path=which("chromedriver"))
+        self.driver.get(linked_in_url)
+        self.url_list = url_list
+
+    def linked_in_signin(self, username, password):
+        # Save current_window as the main_window
+        username = self.driver.find_element_by_name('session_key')
+        username.send_keys(username)
+        password = self.driver.find_element_by_name('session_password')
+        password.send_keys(password)
+        self.driver.find_element_by_class_name('sign-in-form__submit-button').click()
+
+    def loop_through_links(self):
+        ## initialize empty list
+        names, jobs, company, education, location, school  = [], [], [], [], [], []
+        # **Step 3: finding `xpath`** and using skipping-time
+        # For example, you can find the infomation of the `job title` by searching the `inspect` then copy its `full xpath`
+        ## append the obtained values from Selector function
+        T0 = time.time()
+        for k in range(len(self.url_list)):
+            t0 = time.time()
+            path = self.url_list[k]
+            self.driver.get(path)
+            sel = Selector(text=self.driver.page_source)     
+            names.append(sel.xpath("/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[1]/section/div[2]/div[2]/div[1]/ul[1]/li[1]/text()").extract_first())
+            jobs.append( sel.xpath("/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[1]/section/div[2]/div[2]/div[1]/h2/text()").extract_first() )
+            company.append(sel.xpath('/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[1]/section/div[2]/div[2]/div[2]/ul/li[1]/a/span/text()').extract_first())
+            education.append(sel.xpath('/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[2]/div[5]/span/div/section/div[2]/section/ul/li[1]/div/div/a/div[2]/div/p[1]/span[2]/text()').extract_first())
+            location.append(sel.xpath('/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[1]/section/div[2]/div[2]/div[1]/ul[2]/li[1]/text()').extract_first())
+            school.append(sel.xpath('/html/body/div[7]/div[3]/div/div/div/div/div[2]/main/div[2]/div[5]/span/div/section/div[2]/section/ul/li[1]/div/div/a/div[2]/div/h3/text()').extract_first())
+            print("step = %s, appending complete after %s (seconds) "%(k, time.time() - t0))
+            # waiting
+            if (k+1) % 10 == 0:
+                sleep(5)
+
+        ## Step 4. Create dataframe
+        
+        df = pd.DataFrame({'name': names, 
+                        'job title': jobs, 
+                        'company': company, 
+                        'education': education, 
+                        'school': school, 
+                        'location': location, 
+                        'url': self.url_list}) 
+
+        for col in df.columns:
+            df[col] = df[col].apply(lambda x : self.remove(x))
+        return df
+    def remove(self,x):
+        if x == None:
+            x = None
+        else:
+            x = x.replace('\n\t', '').replace('\n', '')
+        return x
+
+
+if __name__ == '__main__':
+    # load data frame
+    df = pd.read_csv("data.csv")
+    email_list = df['Email']  ## Noting that df is your loaded-dataframe of "Customer information"
+    outlook_account = 'thanosteamk09@gmail.com'
+    outlook_password = 'thaNos99*'
+    linkedin_account = 'thanosteamk09@gmail.com'
+    linkedin_password = 'thaNos99*'
+    linked_url = 'https://www.linkedin.com'
+    # driver.get method() will navigate to a page given by the URL address
+    outlook = LinkedIn_URL('https://outlook.live.com/owa/')
+    # outlook.get_site_info()
+    outlook.outlook_signin(outlook_account, outlook_password)
+    outlook.linked_in_signin(linkedin_account, linkedin_password)
+    URL_list = outlook.get_url(email_list)
+    # Close driver
+
+    # Scrap from linked in URL
+    url_scrap = Url_Scrap(linked_url, URL_list)
+    url_scrap.linked_in_signin(linkedin_account, linkedin_password)
+    url_scrap.loop_through_links()
+    outlook.driver.close()
